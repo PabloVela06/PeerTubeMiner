5e4c70a2ab05e208cc27a842f4acb0a39c207732
Hugo Párraga <hugpardel@alum.us.es>
2026-05-04 19:47:18 +0200

    Add files via upload

---
+++ PeerTubeMiner/src/main/java/aiss/peertubeminer/PeerTubeMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.peertubeminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerTubeMinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerTubeMinerApplication.class, args);
+    }
+
+}
