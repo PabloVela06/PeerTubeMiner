6353ab1493339d56374e60c1e31e3346f8670c39
IvanCanGom <ivacangom@alum.us.es>
2026-05-07 11:22:04 +0200

    Created try-catch

--- PeerTubeMiner/src/main/java/aiss/peertubeminer/repositories/ChannelRepository.java
+++ PeerTubeMiner/src/main/java/aiss/peertubeminer/repositories/ChannelRepository.java
@@ -6,6 +6,9 @@ import aiss.peertubeminer.model.videominer.*;
 import aiss.peertubeminer.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.UnknownHttpStatusCodeException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,25 +45,39 @@ public class ChannelRepository {
     }
 
     public VMChannel create(String channelHandle, Integer maxVideos, Integer maxComments, String apiKey) {
-        List<VMVideo> completeVideos = new ArrayList<>();
-        VMChannel channel = channelService.postChannel(channelHandle, apiKey);
-        List<Video> videos = videoService.getVideoPeerTube(channelHandle, maxVideos);
-        for(Video video: videos){
-            VMVideo createdVideo = videoService.postVideo(channel.getId(), Transformer.createVMVideo(video), apiKey);
-            VMUser author = userService.postUser(Transformer.createVMUser(video.getUser()), apiKey);
-            List<VMComment> comments = commentService.postComment(video.getId().toString(), createdVideo.getId(), maxComments, apiKey);
-            List<VMCaption> captions = captionService.postCaption(video.getId().toString(), createdVideo.getId(), apiKey);
+        try {
+            List<VMVideo> completeVideos = new ArrayList<>();
+            VMChannel channel = channelService.postChannel(channelHandle, apiKey);
+            List<Video> videos = videoService.getVideoPeerTube(channelHandle, maxVideos);
+            for (Video video : videos) {
+                VMVideo createdVideo = videoService.postVideo(channel.getId(), Transformer.createVMVideo(video), apiKey);
+                VMUser author = userService.postUser(Transformer.createVMUser(video.getUser()), apiKey);
+                List<VMComment> comments = commentService.postComment(video.getId().toString(), createdVideo.getId(), maxComments, apiKey);
+                List<VMCaption> captions = captionService.postCaption(video.getId().toString(), createdVideo.getId(), apiKey);
 
-            createdVideo.setAuthor(author);
-            createdVideo.setComments(comments);
-            createdVideo.setCaptions(captions);
-            completeVideos.add(createdVideo);
+                createdVideo.setAuthor(author);
+                createdVideo.setComments(comments);
+                createdVideo.setCaptions(captions);
+                completeVideos.add(createdVideo);
 
-            createdVideo = videoService.updateVideo(createdVideo, apiKey);
+                createdVideo = videoService.updateVideo(createdVideo, apiKey);
+            }
+            channel.setVideos(completeVideos);
+            channel = channelService.updateChannel(channel, apiKey);
+            return channel;
         }
-        channel.setVideos(completeVideos);
-        channel = channelService.updateChannel(channel, apiKey);
-        return channel;
+        catch (HttpClientErrorException error) {
+            System.err.println("Client error " + error);
+            return null;
+        }
+        catch (HttpServerErrorException error) {
+            System.err.println("Server error " + error);
+            return null;
+        }
+        catch (UnknownHttpStatusCodeException error) {
+            System.err.println("Unknown error " + error);
+            return null;
+        }
     }
 
 }
